1bfb0fc462bc32190167a6c545d371143f889842
usman362 <alidevelopement01@gmail.com>
2024-05-25 06:40:43 +0500

    changes

--- app/Http/Controllers/AudioController.php
+++ app/Http/Controllers/AudioController.php
@@ -11,9 +11,9 @@ class AudioController extends Controller
         if ($request->hasFile('audio')) {
             $audio = $request->file('audio');
             $filename = time() . '-' . $audio->getClientOriginalName();
-            $path = $audio->storeAs('public/audios/poi-audios', $filename);
-            $relativePath = str_replace('public/', '', $path);
-            return response()->json(['success' => true, 'path' => $relativePath]);
+            $path = $audio->storeAs('audios/poi-audios', $filename);
+            // $relativePath = str_replace('public/', '', $path);
+            return response()->json(['success' => true, 'path' => $path]);
         }
 
         return response()->json(['success' => false, 'message' => 'No audio file uploaded.']);
